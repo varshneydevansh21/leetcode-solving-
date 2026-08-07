25fc041f964675c8b775cc217207a4714cb01cc0
Devansh Varshney <varshney.devansh21@gmail.com>
2026-08-07 12:20:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= 0:
+            if str(x) == str(x)[::-1]:
+                return True
+            else:
+                return False
+        else:
+            return False
+        return bool
